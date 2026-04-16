1f2c701d326bdda5bbd260ae4b90c5bad3d2cf38
kidusw <kidusworkneh555@gmail.com>
2026-04-16 22:02:20 +0300

    deployment

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
-    base: './Porfolio-site/',
+    base: '/Porfolio-site/',
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
